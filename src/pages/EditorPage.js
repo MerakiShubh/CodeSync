96fb04d41a24c94d5bbb0f25a919919a1c37037c
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-10 23:24:31 +0530

    setup for socket io

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Client from "../components/Client";
 import Editor from "../components/Editor";
+import { initSocket } from "../socket.js";
+import ACTIONS from "./Actions.js";
+import { useLocation } from "react-router-dom";
 const EditorPage = () => {
+  const socketRef = useRef(null);
+  const location = useLocation();
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      // socketRef.current.emit(ACTIONS.JOIN, {
+      //   roodId,
+      //   username: location.state?.username,
+      // });
+    };
+    init();
+  }, []);
+
   const [clients, setClients] = useState([
     { socketId: 1, username: "Rakesh k" },
-    // { socketId: 2, username: "John doe" },
+    { socketId: 2, username: "John doe" },
   ]);
   return (
     <div className="mainWrap">
